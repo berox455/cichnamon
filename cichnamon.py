2f25ebc31c0253ec0e1de642d2be871d1edae1d1
berox455 <167811151+berox455@users.noreply.github.com>
2025-05-10 03:36:45 +0200

    Update cichnamon.py - this is a test

--- cichnamon.py
+++ cichnamon.py
@@ -1742,5 +1742,6 @@ game()
 #rework moves
 #maybe could add trading with cichnamon
 #add cichbox loading into trainer_load
+#test
 
 #pouziti gpt na zjednoduseni tutorialu a napsani komandů na jeho vypsani, ktere jsem si potom upravil podle potreb
